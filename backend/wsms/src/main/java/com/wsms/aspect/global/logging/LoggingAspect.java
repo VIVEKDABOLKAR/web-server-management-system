f16c3dc8e7ac020d8ee4855957da980635a70b2b
VIVEK DABOLKAR <dabolkarvivek@gmail.com>
2026-04-13 11:05:42 +0530

    Refactor README for clarity, update application properties for database connection, enhance logging configuration

--- backend/wsms/src/main/java/com/wsms/aspect/global/logging/LoggingAspect.java
+++ backend/wsms/src/main/java/com/wsms/aspect/global/logging/LoggingAspect.java
@@ -13,17 +13,30 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class LoggingAspect {
 
+    private static final String DEFAULT_TRACE_ID = "NO_TRACE";
+    private static final String DEFAULT_REQUEST_ID = "NO_REQUEST";
+
+    /**
+     * around aspect for logs of controller
+     * 
+     * @param joinPoint
+     * @return
+     * @throws Throwable
+     */
     @Around("execution(* com.wsms.controller..*(..))")
     public Object aroundController(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        String traceId = resolveTraceId();
+        String requestId = resolveRequestId();
 
         long start = System.currentTimeMillis();
 
         String className = joinPoint.getSignature().getDeclaringTypeName();
         String methodName = joinPoint.getSignature().getName();
 
-        String traceId = MDC.get("traceId");
-        String requestId = MDC.get("requestId");
-
+        log.info(
+                "traceId={} requestId={} event=controller_call stage=ENTRY layer=controller class={} method={} message=\"Started\"",
+                traceId, requestId, className, methodName);
 
         try {
             Object result = joinPoint.proceed();
@@ -31,18 +44,17 @@ public class LoggingAspect {
             long duration = System.currentTimeMillis() - start;
 
             log.info(
-                    "traceId={} requestId={} event=controller_call layer=controller class={} method={}  outcome=SUCCESS statusCode={} durationMs={} message=\"Success\"",
-                    traceId, requestId, className, methodName, 200, duration
-            );
+                    "traceId={} requestId={} event=controller_call stage=EXIT layer=controller class={} method={}  outcome=SUCCESS statusCode={} durationMs={} message=\"Success\"",
+                    traceId, requestId, className, methodName, 200, duration);
 
             return result;
 
-        } catch (Exception ex) {
+        } catch (Throwable ex) {
 
             long duration = System.currentTimeMillis() - start;
 
             log.error(
-                    "traceId={} requestId={} event=controller_call layer=controller class={} method={} outcome=FAILURE statusCode={} durationMs={} errorType={} message=\"{}\"",
+                    "traceId={} requestId={} event=controller_call stage=EXIT layer=controller class={} method={} outcome=FAILURE statusCode={} durationMs={} errorType={} message=\"{}\"",
                     traceId,
                     requestId,
                     className,
@@ -50,13 +62,19 @@ public class LoggingAspect {
                     500,
                     duration,
                     ex.getClass().getSimpleName(),
-                    ex.getMessage()
-            );
+                    ex.getMessage());
 
             throw ex;
         }
     }
 
+    /**
+     * around aspect for logs of controller
+     * 
+     * @param joinPoint
+     * @return
+     * @throws Throwable
+     */
     @Around("execution(* com.wsms.service..*(..))")
     public Object aroundService(ProceedingJoinPoint joinPoint) throws Throwable {
 
@@ -65,9 +83,19 @@ public class LoggingAspect {
         String className = joinPoint.getSignature().getDeclaringTypeName();
         String methodName = joinPoint.getSignature().getName();
 
-        String traceId = MDC.get("traceId");
-        String requestId = MDC.get("requestId");
+        String traceId = resolveTraceId();
+        String requestId = resolveRequestId();
 
+        // TODO :- this is not production grid, later we will print this log into
+        // internal call
+        // for now we are just ignoring internally calls
+        if (traceId == DEFAULT_TRACE_ID || requestId == DEFAULT_REQUEST_ID) {
+            return joinPoint.proceed();
+        }
+
+        log.info(
+                "traceId={} requestId={} event=service_call stage=ENTRY layer=service class={} method={} message=\"Started\"",
+                traceId, requestId, className, methodName);
 
         try {
             Object result = joinPoint.proceed();
@@ -75,18 +103,17 @@ public class LoggingAspect {
             long duration = System.currentTimeMillis() - start;
 
             log.info(
-                    "traceId={} requestId={} event=service_call layer=service class={} method={} outcome=SUCCESS statusCode={} durationMs={} message=\"Success\"",
-                    traceId, requestId, className, methodName, 200, duration
-            );
+                    "traceId={} requestId={} event=service_call stage=EXIT layer=service class={} method={} outcome=SUCCESS statusCode={} durationMs={} message=\"Success\"",
+                    traceId, requestId, className, methodName, 200, duration);
 
             return result;
 
-        } catch (Exception ex) {
+        } catch (Throwable ex) {
 
             long duration = System.currentTimeMillis() - start;
 
             log.error(
-                    "traceId={} requestId={} event=service_call layer=service class={} method={} outcome=FAILURE statusCode={} durationMs={} errorType={} message=\"{}\"",
+                    "traceId={} requestId={} event=service_call stage=EXIT layer=service class={} method={} outcome=FAILURE statusCode={} durationMs={} errorType={} message=\"{}\"",
                     traceId,
                     requestId,
                     className,
@@ -94,13 +121,19 @@ public class LoggingAspect {
                     500,
                     duration,
                     ex.getClass().getSimpleName(),
-                    ex.getMessage()
-            );
+                    ex.getMessage());
 
             throw ex;
         }
     }
 
+    /**
+     * around aspect for logs of controller
+     * 
+     * @param joinPoint
+     * @return
+     * @throws Throwable
+     */
     @Around("execution(* com.wsms.repository..*(..))")
     public Object aroundRepository(ProceedingJoinPoint joinPoint) throws Throwable {
 
@@ -109,9 +142,19 @@ public class LoggingAspect {
         String className = joinPoint.getSignature().getDeclaringTypeName();
         String methodName = joinPoint.getSignature().getName();
 
-        String traceId = MDC.get("traceId");
-        String requestId = MDC.get("requestId");
+        String traceId = resolveTraceId();
+        String requestId = resolveRequestId();
 
+        // TODO :- this is not production grid, later we will print this log into
+        // internal call
+        // for now we are just ignoring internally calls
+        if (traceId == DEFAULT_TRACE_ID || requestId == DEFAULT_REQUEST_ID) {
+            return joinPoint.proceed();
+        }
+
+        log.info(
+                "traceId={} requestId={} event=repository_call stage=ENTRY layer=repository class={} method={} message=\"Started\"",
+                traceId, requestId, className, methodName);
 
         try {
             Object result = joinPoint.proceed();
@@ -119,18 +162,17 @@ public class LoggingAspect {
             long duration = System.currentTimeMillis() - start;
 
             log.info(
-                    "traceId={} requestId={} event=repository_call layer=repository class={} method={} outcome=SUCCESS statusCode={} durationMs={} message=\"Success\"",
-                    traceId, requestId, className, methodName, 200, duration
-            );
+                    "traceId={} requestId={} event=repository_call stage=EXIT layer=repository class={} method={} outcome=SUCCESS statusCode={} durationMs={} message=\"Success\"",
+                    traceId, requestId, className, methodName, 200, duration);
 
             return result;
 
-        } catch (Exception ex) {
+        } catch (Throwable ex) {
 
             long duration = System.currentTimeMillis() - start;
 
             log.error(
-                    "traceId={} requestId={} event=repository_call layer=repository class={} method={} outcome=FAILURE statusCode={} durationMs={} errorType={} message=\"{}\"",
+                    "traceId={} requestId={} event=repository_call stage=EXIT layer=repository class={} method={} outcome=FAILURE statusCode={} durationMs={} errorType={} message=\"{}\"",
                     traceId,
                     requestId,
                     className,
@@ -138,10 +180,25 @@ public class LoggingAspect {
                     500,
                     duration,
                     ex.getClass().getSimpleName(),
-                    ex.getMessage()
-            );
+                    ex.getMessage());
 
             throw ex;
         }
+    }
+
+    private String resolveTraceId() {
+        String traceId = MDC.get("traceId");
+        if (traceId == null || traceId.isBlank()) {
+            return DEFAULT_TRACE_ID;
+        }
+        return traceId;
+    }
+
+    private String resolveRequestId() {
+        String requestId = MDC.get("requestId");
+        if (requestId == null || requestId.isBlank()) {
+            return DEFAULT_REQUEST_ID;
+        }
+        return requestId;
     }
 }
